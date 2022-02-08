24744d354e01335cf1fb931c2a6b8b686da25022
Tran Quan <tranquan@users.noreply.github.com>
2022-02-08 23:20:22 +0100

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  arrowParens: "avoid",
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  parser: "typescript",
+  printWidth: 120,
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "all",
+  arrowParens: "avoid",
+  useTabs: true,
+};
